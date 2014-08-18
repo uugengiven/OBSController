0be431be9be8a548cb82a4296386092360aa4183
uugengiven <lange.john.m@gmail.com>
2014-08-18 19:54:23 -0400

    Initial commit
    Just grabbed a few links to start building - tomorrow I build

---
+++ OBSController/Controllers/VideoController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace OBSController.Controllers
+{
+    public class VideoController : ApiController
+    {
+
+        // This is where videos will be started, stoppped and the status gotten
+
+        // run an exe: http://stackoverflow.com/questions/1039297/asp-net-running-an-exe-file
+        // retrieve the pid: http://stackoverflow.com/questions/12892268/getting-pid-of-process-started-by-process-start
+    }
+}
